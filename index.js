41e928425255653a23bd86bb3e71eef3831b45db
superkd37 <krishnadas100033@gmail.com>
2021-05-17 23:10:50 +0530

    DB deletion

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 
-const db = require('db')
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
-
 const bot = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
@@ -17,5 +10,5 @@ const bot = new App({
   // Start the app
   await bot.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('Bolt app is running!');
 })();
